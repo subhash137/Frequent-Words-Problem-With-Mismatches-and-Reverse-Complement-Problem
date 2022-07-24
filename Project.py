37fd974baa6b75d33be60b69e5946c0ce07155d9
subhash <96306845+subhash137@users.noreply.github.com>
2022-07-24 20:59:40 +0530

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,4 +1,3 @@
-from itertools import product
 def split_kmer(Text,k):
     return [Text[i:i+k] for i in range(len(Text)-k+1)]
 def reverse_compliment(Pattern):
@@ -18,16 +17,20 @@ def hamming_distance(String1,String2):
 def Approximate_pattern_count(Pattern,kmers,d):
     return sum(1 for i in kmers if hamming_distance(Pattern,i)<=d)
 def generate_Pattern(k):
-    return ["".join(p) for p in product(["A","T","G","C"],repeat=k)]  
-def Frequent_words_with_mismatches(Text,k,d):
+    combinations=['A','T','C','G']
+    for a in range(k-1):
+        combinations = [i+j for i in combinations for j in ['A','T','G','C']]
+    return combinations
+def Frequent_words_with_mismatches_and_reverse_complement(Text,k,d):
     count_pattern={}
     output=[]
     Patterns = generate_Pattern(k)
     kmers = split_kmer(Text,k)
+    max_Count=0
     for Pattern in Patterns:
         reverse = reverse_compliment(Pattern)
         count_pattern[Pattern]=Approximate_pattern_count(Pattern,kmers,d)+Approximate_pattern_count(reverse,kmers,d)
-    max_Count = max(count_pattern.values())
+        max_Count = count_pattern[Pattern] if count_pattern[Pattern]>max_Count else max_Count
     output = [Pattern for Pattern in count_pattern if count_pattern[Pattern]==max_Count]
     print(*output,end=' ')
 File_name = input()
@@ -36,4 +39,4 @@ with open(File_name,'r') as f:
     Text = List[0].strip()
     k = int((List[1].strip()).split(" ")[0])
     d = int((List[1].strip()).split(" ")[1])
-    Frequent_words_with_mismatches(Text,k,d)
+    Frequent_words_with_mismatches_and_reverse_complement(Text,k,d)
